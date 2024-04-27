b36c087084a83ac8a79f6704defda96da533bc45
Samuel Carballo Pacheco <scarballo@intermarkit.es>
2024-04-27 13:22:08 +0200

    Creacion formulario, falta la logica

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,15 +1,49 @@
+import { useState } from "react"
+import { categories } from "../Data/Categories"
 const Form = () => {
+
+    const [activity, setActivity] = useState({
+        category: 2,
+        name: '',
+        calories: 0
+    })
+
     return (
         <form
         className="space-y-5 bg-white shadow p-10 rounded-lg "
         >
             <div className="grid grid-cols-1 gap-3">
-                <label htmlFor="category">Categoria:</label>
+                <label htmlFor="category" className="font-bold">Categoria:</label>
                 <select
                 className="border bordel-slate-300 p-2 rounded-lg w-full bg-white"
                 id="category"
-                ></select>
+                value={activity.category}
+                >
+                    {categories.map(category =>(
+                        <option key={category.id}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
             </div>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="name" className="font-bold">Actividad:</label>
+                <input id="name" className="border bordel-slate-300 p-2 rounded-lg w-full bg-white"
+                        placeholder="Ej. Comidas, Cardio, Entrenamiento"
+                        value={activity.name}
+                />
+            </div>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">Calorias:</label>
+                <input id="calories" className="border bordel-slate-300 p-2 rounded-lg w-full bg-white"
+                type="number"
+                placeholder="Ej. 508"
+                value={activity.calories}
+                />
+            </div>
+            <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-sm"
+                    value='Guardar comida o ejercicio'
+            />
         </form>
     )
 }
---
+++ src/Data/Categories.ts
@@ -0,0 +1,6 @@
+import type { Category } from "../Types/idex"
+
+export const categories : Category[] = [
+    { id: 1, name: 'Comida' },
+    { id: 2, name: 'Ejercicio' }
+]
